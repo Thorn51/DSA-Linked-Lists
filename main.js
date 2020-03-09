46dab6ed2a4814c883b3271a5ef4cfa2d4c393f9
Matt Thornberry <mtberry16@hotmail.com>
2020-03-09 05:39:10 -0600

    Add print values method

--- main.js
+++ main.js
@@ -8,11 +8,13 @@ class _Node {
 class LinkedList {
   constructor() {
     this.head = null;
+    this.size = 0;
   }
 
   //Insert a value at index 0 of linked list
   insertBeginning(data) {
     this.head = new _Node(data, this.head);
+    this.size++;
   }
 
   //Insert a value at the end of the linked list
@@ -25,16 +27,30 @@ class LinkedList {
         tempNode = tempNode.next;
       }
       tempNode.next = new _Node(data, null);
+      this.size++;
     }
   }
 
   //Insert at a specific index of the linked list
   insertAt(data, index) {}
+
+  // Print list data
+  printListData() {
+    let current = this.head;
+
+    while (current) {
+      console.log(current.value);
+      current = current.next;
+    }
+  }
 }
 
 let testList = new LinkedList();
 
 testList.insertBeginning(50);
-testList.insertEnd(75);
+testList.insertBeginning(75);
+testList.insertBeginning(100);
 
 console.log(testList);
+
+testList.printListData();
