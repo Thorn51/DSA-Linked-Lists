630f6885da75617e11de372a0f4edad52415cf04
Matt Thornberry <mtberry16@hotmail.com>
2020-03-08 20:04:42 -0600

    stub insertion methods

--- main.js
+++ main.js
@@ -9,4 +9,13 @@ class LinkedList {
   constructor() {
     this.head = null;
   }
+
+  //Insert a value at index 0 of linked list
+  insertBeginning(data) {}
+
+  //Insert a value at the end of the linked list
+  insertEnd(data) {}
+
+  //Insert at a specific index of the linked list
+  insertAt(data, index) {}
 }
